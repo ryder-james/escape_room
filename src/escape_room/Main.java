25a9c86c090b3ddedd66ecc34a0555f6f3d5707c
Ryder James <nickamilion@yahoo.com>
2018-09-26 16:46:39 -0600

    Enable dynamic file reading

--- src/escape_room/Main.java
+++ src/escape_room/Main.java
@@ -6,6 +6,8 @@ import java.util.Scanner;
 
 public class Main {
 
+	private static final String ROOT_PATH = "src" + File.separator + "escape_room" + File.separator;
+	
 	public static void main(String[] args) {
 		String user = "Cypher";
 		String input = "";
@@ -13,6 +15,9 @@ public class Main {
 
 		System.out.println("Welcome Dr. " + user);
 
+		File[] files = new File(ROOT_PATH + user.toLowerCase())
+				.listFiles();
+		
 		while (input != "exit") {
 			System.out.println("-----------------");
 			System.out.println("Type \"logs\" to view available logs, or type the name of the log to open, "
@@ -21,13 +26,24 @@ public class Main {
 			System.out.println();
 			switch (input) {
 			case "logs":
-				File[] files = new File(user.toLowerCase()).listFiles();
 				for (File file : files) {
-					System.out.println(file.getName());
+					String str = file.getName().substring(0, file.getName().indexOf('.'));
+					System.out.println(str);
 				}
 				break;
+			case "exit":
+				System.out.println("Goodbye");
+				input = "exit";
+				break;
 			default:
-				System.out.println("Unknown option.");
+				for (File file : files) {
+					if (input.equals(file.getName().substring(0, file.getName().indexOf('.')))) {
+						printFile(file.getAbsolutePath());
+						break;
+					}
+				}
+				
+				System.out.println("Unknown option");
 			}
 		}
 
@@ -35,7 +51,7 @@ public class Main {
 
 	}
 
-	private void printFile(String fileName) {
+	private static void printFile(String fileName) {
 		Scanner fileIn;
 		try {
 			fileIn = new Scanner(new File(fileName));
